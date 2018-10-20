e89f0c860911216d1cff1a70a2d2ca9370fe8d35
flamesabor <vysakhul@gmail.com>
2018-10-20 12:03:42 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return 'Hello'
+
+@app.route('/about')
+def about():
+    return 'About Us'
+
+if(__name__=='__main__'):
+    app.run(debug=True)
