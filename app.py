2c657c8842401fc8f4f0e702122afce064756d1d
flamesabor <vysakhul@gmail.com>
2018-10-20 14:51:38 +0530

    Second commit

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from data import Faculties
 
 app = Flask(__name__)
 
-
+myFaculties=Faculties()
 
 @app.route('/')
 def index():
-    return 'Hello'
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
-    return 'About Us'
+    return render_template('about.html')
+
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+
+    if (request.method=='POST'):
+
+        getname=request.form['name']
+        getemail=request.form['emailid']
+        return render_template('contact.html', name=getname, emailid=getemail)
+
+
+@app.route('/faculties')
+def faculties():
+    return render_template('faculties.html', sendfact=myFaculties)
 
 if(__name__=='__main__'):
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,23 @@
+def Faculties():
+    faculties=[
+        {
+            'dept':'Computer Science and Engineering',
+            'name':'Rajesh',
+            'mob':'98951234546',
+            'email':'raj@gmail.com'
+        },
+        {
+            'dept':'Rocket Science and Engineering',
+            'name':'Purusho',
+            'mob':'98951234546',
+            'email':'Purushu@gmail.com'
+        },
+        {
+            'dept':'Chemical Engineering',
+            'name':'Kochunni',
+            'mob':'98951234546',
+            'email':'kochu@gmail.com'
+        }
+    ]
+
+    return faculties
